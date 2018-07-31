375e69a9c56f0941d7606722f0fec4d173e3ae55
nergdvnlt <tylerlundgren@yahoo.com>
2018-07-31 11:25:50 -0600

    Adding TDD for user login.

---
+++ spec/features/user_can_login_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'an existing user can login' do
+  describe 'when they enter their username' do
+    it 'at /welcome' do
+      visit '/welcome'
+
+      click_on 'Login'
+      fill_in 'Username', with: 'fatherWar'
+      fill_in 'Phone Number', with: '+17196639883'
+      click_on 'Login'
+
+      expect(current_path).to eq('/users/1')
+      expect(page).to have_content('fatherWar')
+    end
+  end
+end
